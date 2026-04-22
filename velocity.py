cc44d1e91841933994c2c177ac3e7f6a141cee8f
August Durschinger <snowyowl430@gmail.com>
2026-04-21 22:51:46 -0400

    Radius
    As of this version, the equation for radius of the balloon on mars works.

--- velocity.py
+++ velocity.py
@@ -7,29 +7,18 @@ precision = 0.01        # Time interval
 recursion_depth = 120   # Time limit, seconds
 
 constants = {
-    "g": 9.81,           # Gravitational acceleration, m/s^2
-    "Cd": 0.47,          # Drag coefficient for a sphere
-    "r": 0.1,            # Radius of the balloon, m
-    "rho": 1.225,        # Air density at sea level, kg/m^3
-    "lambda": 0.7,       # Area density of the balloon, kg/m^2
+    "g": 9.81,                          # Gravitational acceleration, m/s^2
+    "Cd": 0.47,                         # Drag coefficient for a sphere
+    "rho": 1.225,                       # Air density at sea level, kg/m^3
+    "rhom": 0.02,                       # Density of the martian atmosphere, kg/m^3
+    "Pm": 610,                          # Pressure of atmosphere on mars, Pa
+    "R": 8.314,                         # Universal gas constant, J/(mol*K)
+    "T": 210,                           # Temperature on mars, K
+    "M": 0.002,                         # Molar mass of diatomic hydrogen, g/mol
+    "rhog": 0.0899,                     # Density of diatomic helium gas, kg/m^3
+    "lambda": 0.7,                      # Area density of the balloon, kg/m^2
 }
 
-def get_acceleration():
+radius = (3 * constants["lambda"]) / (constants["rhom"] - ((constants["Pm"] * (constants["M"]) / (constants["R"] * constants["T"]))))
 
-    # a = (Fb - mg - Fd) / m
-
-    a = constants["g"] - (0.5 * constants["Cd"] * constants["A"] * constants["rho"] * v**2) / constants["m"]
-
-    return a
-
-def main():
-
-    for i in range(recursion_depth/precision):
-
-        a = get_acceleration(v)
-        v += a * precision
-
-        print(f"Time: {i*precision:.2f} s, Velocity: {v:.2f} m/s")
-
-    if __name__ == "__main__":
-        main()
+print(radius)
